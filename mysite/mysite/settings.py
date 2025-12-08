ac3c1557b82a04ca581605ba3c6aebb6a0605f10
PoomrapeeS <poomrapee.s@outlook.co.th>
2025-12-08 19:52:44 +0700

    Use Postgres instead sqlite and define .env varaibles on github secrets

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -93,12 +93,27 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+# Check if we should use PostgreSQL (production) or SQLite (development)
+USE_POSTGRES = os.environ.get("USE_POSTGRES", "False") == "True"
+
+if USE_POSTGRES:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB", "portfolio"),
+            "USER": os.environ.get("POSTGRES_USER", "postgres"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "changeme"),
+            "HOST": os.environ.get("DB_HOST", "db"),
+            "PORT": os.environ.get("DB_PORT", "5432"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
